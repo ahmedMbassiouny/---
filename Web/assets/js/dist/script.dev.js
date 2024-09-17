305b6f558bffe134b8f5ee6305fea9076d1dfdbe
Ahmed Mohamed Bassiouny <113218302+ahmedMbassiouny@users.noreply.github.com>
2024-09-17 03:55:13 +0300

    Add files via upload

---
+++ Web/assets/js/dist/script.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(document).ready(function () {
+  $("#header").load("../../Layouts/header.html");
+});
+$(document).ready(function () {
+  $("#footer").load("../../Layouts/footer.html");
+});
+$(document).ready(function () {
+  $("#loader").load("../../Layouts/loader.html");
+});
+window.addEventListener("load", function () {
+  document.querySelector("#loader").style.display = "none";
+});
+//# sourceMappingURL=script.dev.js.map
